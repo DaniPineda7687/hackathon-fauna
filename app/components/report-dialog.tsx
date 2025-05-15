57f5055fde6d6c8ec6bf65fec81ad6a1d731384c
Daniel Pineda <danielspineda87@gmail.com>
2025-05-15 14:40:43 -0500

    feat: 🎸 integrate Toaster component and enhance ReportDialog functionality

--- app/components/report-dialog.tsx
+++ app/components/report-dialog.tsx
@@ -1,17 +1,30 @@
 "use client"
 
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer"
+import {
+  Drawer,
+  DrawerTrigger,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerClose,
+} from "@/components/ui/drawer"
 import { PawPrint } from "lucide-react"
 import LocationMap from "./location-map"
+import { toast } from "sonner"
 
 export function ReportDialog() {
+  const [open, setOpen] = useState(false)
 
- 
   return (
-    <Drawer>
+    <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <button className="absolute bottom-5 right-5 bg-red z-10 bg-black p-4 rounded-full text-white shadow-lg cursor-pointer">
+        <button
+          className="absolute bottom-5 right-5 z-10 bg-black p-4 rounded-full text-white shadow-lg"
+        >
           <PawPrint />
         </button>
       </DrawerTrigger>
@@ -22,12 +35,17 @@ export function ReportDialog() {
             <DrawerDescription>Busca la ubicación del avistamiento</DrawerDescription>
           </DrawerHeader>
           <div className="p-4 pb-0">
-            
             <LocationMap />
-            
           </div>
-          <DrawerFooter>
-            <Button>Reportar</Button>
+          <DrawerFooter className="flex gap-2">
+            <Button
+              onClick={() => {
+                toast("Se ha generado el reporte")
+                setOpen(false)
+              }}
+            >
+              Reportar
+            </Button>
             <DrawerClose asChild>
               <Button variant="outline">Cancelar</Button>
             </DrawerClose>
